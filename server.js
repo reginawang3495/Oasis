0980a01d503ae4ab04b62205607559b4294f48f6
Regina Wang <rrreggginnna@gmail.com>
2019-04-07 19:01:20 +0100

    initial commit

--- server.js
+++ server.js
@@ -15,9 +15,9 @@ request.get('https://reginawang99.github.io/Oasis/alienwareapples.txt', function
   }}); // add offender
 
 
-  allText.substring(1, allText.length-1);
-  var data = allText.split("\"\n\"");
-	
+allText.substring(1, allText.length-1);
+var data = allText.split("\"\n\"");
+
 
 //initialize 2D array for crime data with zeros
 var crimeModel = new Array(100);
@@ -28,122 +28,122 @@ for(var i = 0; i < crimeModel.length; i++){
 }
 
 //update crimeModel, 2D array, with number of crime occurrences at each location
-	for (i = 0; i < data.length; i++) {
-		var loc = data[i];
-		var div = loc.indexOf(",");
+for (i = 0; i < data.length; i++) {
+	var loc = data[i];
+	var div = loc.indexOf(",");
 
-		var lat = parseFloat( loc.slice(1,div) );
-		var long = parseFloat( loc.slice(div+1, loc.length-1 ));
+	var lat = parseFloat( loc.slice(1,div) );
+	var long = parseFloat( loc.slice(div+1, loc.length-1 ));
 
 		if ((lat >= 33.5 && lat <= 34.5) && (long <= -118 && long >= -119)) { //check in bounds
 			crimeModel[round( abs(parseFloat( loc.slice(1,div) )-33.5)*100.0 )][round( abs(parseFloat( loc.slice(div+1, loc.length-1 ))+118)*100.0 )]++; 
 		}
 	}
 
-console.log("inforrrrrr"+[0][0]);
+	console.log("inforrrrrr"+[0][0]);
 
-var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
-var request = require('request');
-console.log("hi");
+	var XMLHttpRequest = require("xmlhttprequest").XMLHttpRequest;
+	var request = require('request');
+	console.log("hi");
 
-function addOffender(name, callback){
-	try{
-	var result = "";
-	var jsonObj = {"name":name };
-	console.log("https://westcentralus.api.cognitive.microsoft.com/face/v1.0/largepersongroups/sexualoffenders/persons");
-	request({
+	function addOffender(name, callback){
+		try{
+			var result = "";
+			var jsonObj = {"name":name };
+			console.log("https://westcentralus.api.cognitive.microsoft.com/face/v1.0/largepersongroups/sexualoffenders/persons");
+			request({
 
-		uri: "https://westcentralus.api.cognitive.microsoft.com/face/v1.0/largepersongroups/sexualoffenders/persons",
-		method: "POST",
-		headers: {
-			"Content-Type": "application/json",
-			"Ocp-Apim-Subscription-Key":"50e122d0a26e468bb683e81f687a3e0d",
-		},
-		json: true,
-		body : jsonObj
+				uri: "https://westcentralus.api.cognitive.microsoft.com/face/v1.0/largepersongroups/sexualoffenders/persons",
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+					"Ocp-Apim-Subscription-Key":"50e122d0a26e468bb683e81f687a3e0d",
+				},
+				json: true,
+				body : jsonObj
 
 
-	},  function(error, response, body){
-		if (!error && response.statusCode == 200){
-			status = "succeeded";
-			result = body.personId;
-			console.log("THIAWHFSJJFKF");
-			callback(result);
-		} else {
-			console.log("THIAWHFSJJFKF");
-			callback(error);
+			},  function(error, response, body){
+				if (!error && response.statusCode == 200){
+					status = "succeeded";
+					result = body.personId;
+					console.log("THIAWHFSJJFKF");
+					callback(result);
+				} else {
+					console.log("THIAWHFSJJFKF");
+					callback(error);
 
+				}
+			}
+			);
 		}
+		catch(error){}
 	}
-	);
-}
-catch(error){}
-}
 
-function addFace(id, url){
-	try{
-	var jsonObj = {"url":url };
-	console.log(url);
-	request({
+	function addFace(id, url){
+		try{
+			var jsonObj = {"url":url };
+			console.log(url);
+			request({
 
-		uri: "https://westcentralus.api.cognitive.microsoft.com/face/v1.0/largepersongroups/sexualoffenders/persons/"+id+"/persistedfaces",
-		method: "POST",
-		headers: {
-			"Content-Type": "application/json",
-			"Ocp-Apim-Subscription-Key":"50e122d0a26e468bb683e81f687a3e0d",
-		},
-		json: true,
-		body :jsonObj
-	}, function(error, response, body){
-		if (!error && response.statusCode == 200){
-			console.log('message sent successfully');
+				uri: "https://westcentralus.api.cognitive.microsoft.com/face/v1.0/largepersongroups/sexualoffenders/persons/"+id+"/persistedfaces",
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+					"Ocp-Apim-Subscription-Key":"50e122d0a26e468bb683e81f687a3e0d",
+				},
+				json: true,
+				body :jsonObj
+			}, function(error, response, body){
+				if (!error && response.statusCode == 200){
+					console.log('message sent successfully');
 
-		} else {
-			console.log('error ==2 ' + error);
+				} else {
+					console.log('error ==2 ' + error);
+				}
+			});
+		}catch(error){}
+	}
+
+	function train(){
+		try{
+			request({
+				uri: "https://westcentralus.api.cognitive.microsoft.com/face/v1.0/largepersongroups/sexualoffenders/train",
+				method: "POST",
+				headers: {
+					"Content-Type": "application/json",
+					"Ocp-Apim-Subscription-Key":"50e122d0a26e468bb683e81f687a3e0d",
+				},
+				json: true
+			}, function(error, response, body){
+				if (!error && response.statusCode == 200){
+					console.log('message sent successfully');
+
+					var ans = JSON.parse(body);
+					console.log("yay trained!");				
+				} else {
+					console.log('error ==3 ' + error);
+				}
+			});
 		}
-	});
-}catch(error){}
-}
-
-function train(){
-	try{
-	request({
-		uri: "https://westcentralus.api.cognitive.microsoft.com/face/v1.0/largepersongroups/sexualoffenders/train",
-		method: "POST",
-		headers: {
-			"Content-Type": "application/json",
-			"Ocp-Apim-Subscription-Key":"50e122d0a26e468bb683e81f687a3e0d",
-		},
-		json: true
-	}, function(error, response, body){
-		if (!error && response.statusCode == 200){
-			console.log('message sent successfully');
-
-			var ans = JSON.parse(body);
-			console.log("yay trained!");				
-		} else {
-			console.log('error ==3 ' + error);
-		}
-	});
-}
-catch(error){}
-}
+		catch(error){}
+	}
 
 
-request.get('https://reginawang99.github.io/Oasis/resources.txt', function (error, response, body) {
-	if (!error && response.statusCode == 200) {
-		var txt = body;
-		console.log("here is txt"+txt);
-		var offenders = txt.split("\n");
-		for(var i = 0; i < offenders.length; i++){
-			var id = "hmm";
-			id =  addOffender(offenders[i].substring(1,offenders[i].indexOf("\",\"")),function(result){
-				if(result){
-					id = result;
-					console.log('something blew up' );
-				} 
+	request.get('https://reginawang99.github.io/Oasis/resources.txt', function (error, response, body) {
+		if (!error && response.statusCode == 200) {
+			var txt = body;
+			console.log("here is txt"+txt);
+			var offenders = txt.split("\n");
+			for(var i = 0; i < offenders.length; i++){
+				var id = "hmm";
+				id =  addOffender(offenders[i].substring(1,offenders[i].indexOf("\",\"")),function(result){
+					if(result){
+						id = result;
+						console.log('something blew up' );
+					} 
   }); // add offender
-			console.log("here is id: "+ i);
+				console.log("here is id: "+ i);
 
         	addFace("a1687612-ba3c-4fbb-9501-3d4f6958a38f", offenders[i].substring(offenders[i].indexOf("\",\"")+3, offenders[i].length-1)); // add face
         }
@@ -236,77 +236,77 @@ request.get('https://reginawang99.github.io/Oasis/resources.txt', function (erro
 		res.send("bad request");
 	});*/
 	app.post('/Danger', (req, res) =>{
-		try{
-		if(req.body.key == "apples"){
-			var phoneNum = req.body.phoneNum;
-			var imageUrl = req.body.imageUrl;
-			var isBADGUY = false;
-			function facial(){
-				var jsonObj1 = {"url":imageUrl };
-				console.log(imageUrl);
-				request({
+			if(req.body.key == "apples"){
+				var phoneNum = req.body.phoneNum;
+				var imageUrl = req.body.imageUrl;
+				var isBADGUY = false;
+				function facial(){
+					var jsonObj1 = {"url":imageUrl };
+					console.log(imageUrl);
+					request({
 
-					uri: "https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect",
-					method: "POST",
-					headers: {
-						"Content-Type": "application/json",
-						"Ocp-Apim-Subscription-Key":"50e122d0a26e468bb683e81f687a3e0d",
-					},
-					json: true,
-					body :jsonObj1
-				}, function(error, response, body){
-					if (!error && response.statusCode == 200){
-						console.log("askdfjaklsdjf;lakdjf");
-						singlefaceid = body[0].faceId;
+						uri: "https://westcentralus.api.cognitive.microsoft.com/face/v1.0/detect",
+						method: "POST",
+						headers: {
+							"Content-Type": "application/json",
+							"Ocp-Apim-Subscription-Key":"50e122d0a26e468bb683e81f687a3e0d",
+						},
+						json: true,
+						body :jsonObj1
+					}, function(error, response, body){
+						if (!error && response.statusCode == 200){
+							console.log("askdfjaklsdjf;lakdjf");
+							singlefaceid = body[0].faceId;
 							console.log(singlefaceid);
 
-						function identify(){
-							var jsonObj = {"faceIds":[singlefaceid],"largePersonGroupId":"sexualoffenders" };
-							console.log(url);
-							request({
+							function identify(){
+								var jsonObj = {"faceIds":[singlefaceid],"largePersonGroupId":"sexualoffenders" };
+								console.log(url);
+								request({
 
-								uri: "https://westcentralus.api.cognitive.microsoft.com/face/v1.0/identify",
-								method: "POST",
-								headers: {
-									"Content-Type": "application/json",
-									"Ocp-Apim-Subscription-Key":"50e122d0a26e468bb683e81f687a3e0d",
-								},
-								json: true,
-								body :jsonObj
-							}, function(error, response, body){
-								console.log("hi");
-								if (!error && response.statusCode == 200){
-									console.log('message sent successfully');
-									try{
-										response[0]
-										isBADGUY =true;
-									}catch(error){										
-										isBADGUY =false;
+									uri: "https://westcentralus.api.cognitive.microsoft.com/face/v1.0/identify",
+									method: "POST",
+									headers: {
+										"Content-Type": "application/json",
+										"Ocp-Apim-Subscription-Key":"50e122d0a26e468bb683e81f687a3e0d",
+									},
+									json: true,
+									body :jsonObj
+								}, function(error, response, body){
+									console.log("hi");
+									if (!error && response.statusCode == 200){
+										console.log('message sent successfully');
+										try{
+											response[0]
+											isBADGUY =true;
+										}catch(error){										
+											isBADGUY =false;
+										}
+
+									} else {
+										console.log('aaaa == ' + response +" dmdmd "+ body + " vdvv "+error);
 									}
+								});
+							}
+							identify();
+							console.log('message sent successfully');
 
-								} else {
-					console.log('aaaa == ' + response +" dmdmd "+ body + " vdvv "+error);
-								}
-							});
+						} else {
+							console.log('aaaeea == '  +" dmdmd "+ body[0] + " vdvv "+error);
 						}
-						identify();
-						console.log('message sent successfully');
 
-					} else {
-					console.log('aaaeea == '  +" dmdmd "+ body[0] + " vdvv "+error);
-					}
-				});
-			}catch(error){}
-			}
-			facial();
+				}
+			);
+		}
+				facial();
 
 
-			function text(){
-				try{
-				var toSend = "There seems to be no sexual offenders in the picture, but stay safe.";
-				if(isBADGUY)
-					toSend = "Be careful! We are pretty sure that there is a sexual offender in that picture!";
-				var BOdyyyy = "Body=" + toSend + "hiii&From=112133400272&To="+phoneNum+"&undefined=";
+				function text(){
+					try{
+						var toSend = "There seems to be no sexual offenders in the picture, but stay safe.";
+						if(isBADGUY)
+							toSend = "Be careful! We are pretty sure that there is a sexual offender in that picture!";
+						var BOdyyyy = "Body=" + toSend + "hiii&From=112133400272&To="+phoneNum+"&undefined=";
 				//var jsonObj = {"Body":url };
 				request({
 					uri: "https://api.twilio.com/2010-04-01/Accounts/AC8d85c1d898fe71ae9a46a016318fbedf/Messages.json",
@@ -331,14 +331,14 @@ request.get('https://reginawang99.github.io/Oasis/resources.txt', function (erro
 				}
 			});
 			}catch(error){}
-			}
-			text();
-
-			res.send("worked!");
-
 		}
-		res.send("bad request");
-	});
+		text();
+
+		res.send("worked!");
+
+	}
+	res.send("bad request");
+});
 	app.get('/hi', function(req, res){
 		res.send("hi! i work still :)");
 	});
