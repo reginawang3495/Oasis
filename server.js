a6b1bdffb3706092112284c73ed1b20768e9ee59
Regina Wang <rrreggginnna@gmail.com>
2019-04-07 18:03:06 +0100

    initial commit

--- server.js
+++ server.js
@@ -215,6 +215,7 @@ request.get('https://reginawang99.github.io/Oasis/resources.txt', function (erro
 				}, function(error, response, body){
 					if (!error && response.statusCode == 200){
 						singlefaceid = body[0].faceId;
+							console.log(singlefaceid);
 
 						function identify(){
 							var jsonObj = {"faceIds":[singlefaceid],"largePersonGroupId":"sexualoffenders" };
